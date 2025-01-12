959878afa9d525ed7ec99c4e77069ee3d9ca771b
Timolej <153808973+TimolejCupcake@users.noreply.github.com>
2025-01-12 23:46:30 +0100

    If you would die the score and difficulty scaling did not change, they now do.

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+input.onButtonPressed(Button.A, function () {
     if (die == 0) {
         basic.clearScreen()
         if (p_pos_x == 2) {
@@ -7,14 +6,11 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
         } else if (p_pos_x == 4) {
             p_pos_x += -2
         } else {
-            
+        	
         }
-        
     }
-    
 })
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    
+input.onButtonPressed(Button.AB, function () {
     if (die == 0) {
         basic.clearScreen()
         if (p_pos_y == 4) {
@@ -22,12 +18,9 @@ input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
         } else {
             p_pos_y = 4
         }
-        
     }
-    
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
+input.onButtonPressed(Button.B, function () {
     if (die == 0) {
         basic.clearScreen()
         if (p_pos_x == 2) {
@@ -35,11 +28,9 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
         } else if (p_pos_x == 0) {
             p_pos_x += 2
         } else {
-            
+        	
         }
-        
     }
-    
 })
 let random = 0
 let score = 0
@@ -50,12 +41,10 @@ let p_pos_y = 0
 p_pos_y = 4
 p_pos_x = 2
 let π = 3.14
-loops.everyInterval(500, function on_every_interval() {
-    
+loops.everyInterval(500, function () {
     time += 1
 })
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
     led.plot(p_pos_x, p_pos_y)
     if (die == 1) {
         basic.clearScreen()
@@ -65,11 +54,11 @@ basic.forever(function on_forever() {
         basic.clearScreen()
         basic.pause(1000)
         die = 0
+        score = 0
+        time = 0
     }
-    
 })
-loops.everyInterval(1100 - time * 100, function on_every_interval2() {
-    
+loops.everyInterval(1100 - time * 100, function () {
     if (die == 0) {
         random = randint(0, 4)
         if (random == 0) {
@@ -87,7 +76,6 @@ loops.everyInterval(1100 - time * 100, function on_every_interval2() {
             if (p_pos_x == 0) {
                 die = 1
             }
-            
             basic.pause(50)
             led.unplot(0, 1)
             basic.pause(50)
@@ -99,7 +87,7 @@ loops.everyInterval(1100 - time * 100, function on_every_interval2() {
             basic.pause(50)
             score += 1
         } else if (random == 1) {
-            for (let index2 = 0; index2 < 20; index2++) {
+            for (let index = 0; index < 20; index++) {
                 basic.pause(50)
                 led.toggle(2, 0)
             }
@@ -113,7 +101,6 @@ loops.everyInterval(1100 - time * 100, function on_every_interval2() {
             if (p_pos_x == 2) {
                 die = 1
             }
-            
             basic.pause(50)
             led.unplot(2, 1)
             basic.pause(50)
@@ -125,7 +112,7 @@ loops.everyInterval(1100 - time * 100, function on_every_interval2() {
             basic.pause(50)
             score += 1
         } else if (random == 2) {
-            for (let index3 = 0; index3 < 20; index3++) {
+            for (let index = 0; index < 20; index++) {
                 basic.pause(50)
                 led.toggle(4, 0)
             }
@@ -139,7 +126,6 @@ loops.everyInterval(1100 - time * 100, function on_every_interval2() {
             if (p_pos_x == 4) {
                 die = 1
             }
-            
             basic.pause(50)
             led.unplot(4, 1)
             basic.pause(50)
@@ -151,7 +137,7 @@ loops.everyInterval(1100 - time * 100, function on_every_interval2() {
             basic.pause(50)
             score += 1
         } else if (random == 3) {
-            for (let index4 = 0; index4 < 20; index4++) {
+            for (let index = 0; index < 20; index++) {
                 basic.pause(50)
                 led.toggle(0, 4)
             }
@@ -165,7 +151,6 @@ loops.everyInterval(1100 - time * 100, function on_every_interval2() {
             if (p_pos_y == 4) {
                 die = 1
             }
-            
             basic.pause(50)
             led.unplot(1, 4)
             basic.pause(50)
@@ -177,7 +162,7 @@ loops.everyInterval(1100 - time * 100, function on_every_interval2() {
             basic.pause(50)
             score += 1
         } else {
-            for (let index5 = 0; index5 < 20; index5++) {
+            for (let index = 0; index < 20; index++) {
                 basic.pause(50)
                 led.toggle(4, 3)
             }
@@ -191,7 +176,6 @@ loops.everyInterval(1100 - time * 100, function on_every_interval2() {
             if (p_pos_x == 3) {
                 die = 1
             }
-            
             basic.pause(50)
             led.unplot(3, 3)
             basic.pause(50)
@@ -203,8 +187,6 @@ loops.everyInterval(1100 - time * 100, function on_every_interval2() {
             basic.pause(50)
             score += 1
         }
-        
         basic.pause(500)
     }
-    
 })
